44c1876257b03cca0f44cdb056bbbb513ee91de5
Zadok Joshua <ayegbazadok@outlook.com>
2022-06-23 15:57:47 +0100

    New update

---
+++ Calculations/Converter/Temperature.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Calculations.Converter
+{
+    public class Temperature
+    {
+        private double _output; 
+
+        public double CelsiusToKelvin(double temperature)
+        {
+            _output = temperature + 273.15;
+            return _output;
+        }
+
+        public double CelsiusToFahrenheit(double temperature)
+        {
+            _output = (temperature * (9 / 5))  + 32;
+            return _output;
+        }
+
+        public double KelvinToFahrenheit(double temperature)
+        {
+            _output = ((temperature - 273.15) *  (9 / 5)) + 32;
+            return _output;
+        }
+
+        public double KelvinToCelsius(double temperature)
+        {
+            _output = temperature - 273.15;
+            return _output;
+        }
+
+        public double FahrenheitToCelsius(double temperature)
+        {
+            _output = (temperature - 32) * 5 / 9;
+            return _output;
+        }
+
+        public double FahrenheitToKelvin(double temperature)
+        {
+            _output = ((temperature - 32) * (5 / 9)) + 273.15;
+            return _output;
+        }
+
+    }
+}
--- Matika/MainWindow.xaml.cs
+++ Matika/MainWindow.xaml.cs
@@ -20,10 +20,72 @@ namespace Matika
     /// </summary>
     public partial class MainWindow : Window
     {
+
         public MainWindow()
         {
             InitializeComponent();
         }
 
+        private void btn_Click(object sender, RoutedEventArgs e)
+        {
+
+            if(sender == sevenBtn)
+            {
+                txtBlock1.Text = $"{txtBlock1.Text}7";
+                
+            }
+            else if (sender == eightBtn)
+            {
+                txtBlock1.Text = $"{txtBlock1.Text}8";
+            }
+            else if (sender == nineBtn)
+            {
+                txtBlock1.Text = $"{txtBlock1.Text}9";
+            }
+            else if (sender == fourBtn)
+            {
+                txtBlock1.Text = $"{txtBlock1.Text}4";
+            }
+            else if (sender == fiveBtn)
+            {
+                txtBlock1.Text = $"{txtBlock1.Text}5";
+            }
+            else if (sender == sixBtn)
+            {
+                txtBlock1.Text = $"{txtBlock1.Text}6";
+            }
+            else if (sender == threeBtn)
+            {
+                txtBlock1.Text = $"{txtBlock1.Text}3";
+            }
+            else if (sender == twoBtn)
+            {
+                txtBlock1.Text = $"{txtBlock1.Text}2";
+            }
+            else if (sender == oneBtn)
+            {
+                txtBlock1.Text = $"{txtBlock1.Text}1";
+            }
+            else if (sender == dotBtn)
+            {
+                txtBlock1.Text = $"{txtBlock1.Text}.";
+            }
+            else if (sender == ceBtn)
+            {
+                txtBlock1.Text = "0";
+            }
+            else if (sender == deleteBtn)
+            {
+                var temp = txtBlock1.Text;
+                var length = temp.Count();
+                temp = txtBlock1.Text.Remove(length - 1);
+                if (txtBlock1.Text != "0") { txtBlock1.Text = temp; }
+            }
+        }
+
+
+
+        
     }
+
 }
